3c4c40a0206db768e22b55a7f26ebc7ae578eaea
suhyouri <suhyouri@gmail.com>
2021-03-28 10:26:44 +0100

    commit on 'main'

---
+++ server.js
@@ -0,0 +1,192 @@
+//server.js
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import * as dat from "dat.gui";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import gsap from "gsap";
+
+
+app.use(express.static(__dirname + "/"));
+app.get("/", function (req, res, next) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+
+//start our web server and socket.io server listening
+server.listen(5500, function () {
+  console.log("server: listening on *:5500");
+});
+
+
+// /**
+//  * Base
+//  */
+// // Debug
+// const gui = new dat.GUI();
+
+// // Canvas
+// const canvas = document.querySelector("canvas.webgl");
+
+// // Scene
+// const scene = new THREE.Scene();
+
+// /**
+//  * Models
+//  */
+
+// const gltfLoader = new GLTFLoader();
+
+// let mixer = null;
+// //let randomNum = random.floor(random.)
+
+// gltfLoader.load("/models/snow/snowman_suhyouri.gltf", (gltf) => {
+  
+//   for (let i = 0; i < 10; i++) {
+
+//     let copy = gltf.scene.clone();
+//     copy.position.x = Math.random()*3;
+//     copy.position.y = Math.random()*3;
+//     copy.position.z = Math.random()*3;
+//     // copy.rotation.x = Math.random() * Math.PI;
+//     // copy.rotation.y = Math.random() * Math.PI;
+
+//     const scale = Math.random() * 0.3;
+//     copy.scale.set(scale, scale, scale);
+//     scene.add(copy);
+
+//     console.log(i);
+
+//     let animate = function () {
+//       copy.rotation.y += 0.01;
+//       renderer.render(scene, camera);
+//       requestAnimationFrame(animate);
+//     };
+//     animate();
+//   }
+
+//   gltf.scene.scale.set(1, 1, 1);
+//   gltf.scene.position.set(0, 0.5, 0);
+//   scene.add(gltf.scene);
+
+//   let animate = function () {
+//     gltf.scene.rotation.y += 0.01;
+//     renderer.render(scene, camera);
+//     requestAnimationFrame(animate);
+//   };
+//   animate();
+// });
+
+// /**
+//  * Floor
+//  */
+// const floor = new THREE.Mesh(
+//   new THREE.PlaneGeometry(10, 10),
+//   new THREE.MeshStandardMaterial({
+//     color: "#00ff00",
+//     metalness: 0,
+//     roughness: 0.5,
+//   })
+// );
+// floor.receiveShadow = true;
+// floor.rotation.x = -Math.PI * 0.5;
+// scene.add(floor);
+
+// /**
+//  * Lights
+//  */
+// const ambientLight = new THREE.AmbientLight(0xffffff, 2);
+// scene.add(ambientLight);
+
+// const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+// directionalLight.position.set(-5, 5, 0);
+// directionalLight.position.set(5, 5, 0);
+// directionalLight.position.set(-5, 5, 5);
+// scene.add(directionalLight);
+
+// /**
+//  * Sizes
+//  */
+// const sizes = {
+//   width: window.innerWidth,
+//   height: window.innerHeight,
+// };
+
+// window.addEventListener("resize", () => {
+//   // Update sizes
+//   sizes.width = window.innerWidth;
+//   sizes.height = window.innerHeight;
+
+//   // Update camera
+//   camera.aspect = sizes.width / sizes.height;
+//   camera.updateProjectionMatrix();
+
+//   // Update renderer
+//   renderer.setSize(sizes.width, sizes.height);
+//   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+// });
+
+// /**
+//  * Camera
+//  */
+// // Base camera
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   sizes.width / sizes.height,
+//   0.1,
+//   100
+// );
+// camera.position.set(2, 2, 2);
+// scene.add(camera);
+
+// // Controls
+// const controls = new OrbitControls(camera, canvas);
+// controls.target.set(0, 0.75, 0);
+// controls.enableDamping = true;
+
+// /**
+//  * Renderer
+//  */
+// const renderer = new THREE.WebGLRenderer({
+//   canvas: canvas,
+// });
+// renderer.shadowMap.enabled = true;
+// renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+// renderer.setSize(sizes.width, sizes.height);
+// renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// /**
+//  * Animate
+//  */
+// const clock = new THREE.Clock();
+// let previousTime = 0;
+
+// const tick = () => {
+//   const elapsedTime = clock.getElapsedTime();
+//   const deltaTime = elapsedTime - previousTime;
+//   previousTime = elapsedTime;
+
+//   // Model animation
+//   if (mixer) {
+//     mixer.update(deltaTime);
+//   }
+//   //gsap.to(gltfLoader.position, {duration:1, delay: 1, x:2})
+//   //floor.rotation.z = 0.1 * elapsedTime;
+//   gsap.to(floor, { duration: 1, delay: 1, z: 2 });
+
+//   // Update controls
+//   controls.update();
+
+//   // Render
+//   renderer.render(scene, camera);
+
+//   // Call tick again on the next frame
+//   window.requestAnimationFrame(tick);
+// };
+
+// tick();
